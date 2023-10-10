9f53eb3b9d551f8330e3b12427beeabce860dc4e
Ermis <cydisalt@gmail.com>
2023-10-11 00:44:25 +0300

    npcs can talk now

--- src/systems/npcs.ts
+++ src/systems/npcs.ts
@@ -25,20 +25,48 @@ export default function npcs(dt: number, display: Display) {
       };
       const split = e.npc.text.split('|');
       let delay = 0;
-      for (const s of split) {
+      let i = 0;
+      for (let s of split) {
         const res = /<wait:([0-9]+)>/gm.exec(s);
         const time = Number(res?.[1]);
         if (!Number.isNaN(time)) delay += time;
 
-        const part = new Text(s);
+        s = s.replace(/<wait:([0-9]+)>/gm, '');
+
+        const part = new Text(s.trim(), {
+          fontFamily: "Mario",
+          fill: 'white',
+          strokeThickness: 5
+        });
+        part.visible = false;
+        part.scale.set(0.2);
+        part.zIndex = 100;
         part.position.x = e.position.x;
-        part.position.y = e.position.y;
+        part.position.y = e.position.y - e.size.y * 0.5 - 8 - (split.length - 1 - i) * part.height;
+        part.anchor.set(0.5, 1);
         display.add(part);
 
         e.npc.parsed.text.push({
           delay,
           part
         });
+        i++;
+      }
+    }
+
+    const close = entities.view(['mario']).some(m => m.position.distance(e.position) < 32);
+
+    if (close) {
+      e.npc.parsed.t += dt;
+    } else {
+      e.npc.parsed.t = 0;
+    }
+
+    for (const t of e.npc.parsed.text) {
+      if (t.delay < e.npc.parsed.t) {
+        if (!t.part.visible) t.part.visible = true;
+      } else {
+        if (t.part.visible) t.part.visible = false;
       }
     }
   }
